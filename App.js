787d17bdb4cfd474e846577f4640d50a87a4c7f1
Risho <71736133+risho7744@users.noreply.github.com>
2021-01-05 14:35:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import Row from './Row';
+import './App.css';
+import requests from './requests';
+import Banner from './Banner';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+
+      <NavBar />
+
+      <Banner />
+
+      <Row 
+      title="NETFLIX ORIGINALS" 
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+      />
+      
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchToprated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+    </div>
+  );
+}
+
+export default App;
